ce2da836aa3a351f5beba0cc8f2dfae7832f9142
Gaurav <pushpamgaurav@gmail.com>
2023-11-28 17:51:36 +0530

    2

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ passwort.use(new googleStrategy({
   console.log(profile);
 }));
 
-app.get('/auth/google', passwort.authenticate('google', {
+app.get('/', passwort.authenticate('google', {
   scope: ["profile", "email"]
 }))
 
-// app.get('/auth/google/callback', passwort.authenticate('google'));
-const x = require('./controllers/userControllers');
-app.get('/auth', x.user_login);
+app.get('/auth/google/callback', passwort.authenticate('google'));
+
+const myUser = require('./controllers/userControllers');
+app.get('/auth/google/callback', myUser.user_login);
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/myLonexRentalProject');
+// mongoose.connect('mongodb://127.0.0.1:27017/myLonexRentalProject');
 
 //user routes
 const user_route = require('./routes/userRoutes');
@@ -34,6 +35,21 @@ const { access } = require('fs');
 app.use('/api', user_route)
 
 
-app.listen(3000, function () {
-  console.log("Server is ready");
-})
+// app.listen(3000, function () {
+//   console.log("Server is ready");
+// })
+
+const PORT = 8000;
+const DB = "mongodb+srv://Gaurav:eP2ILjAadWqdYhMda@gaurav.hxxlayo.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(`Server is running on :${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error("Error connecting to MongoDB:", error);
+  });
